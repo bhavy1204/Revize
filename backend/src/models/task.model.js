eadee523e0e9d8d2abf954731aeb0c2b325fd012
bhavy1204 <bhavysarwa51@gmail.com>
2026-01-06 08:01:55 +0530

    feat: implement task and revision models

---
+++ backend/src/models/task.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const revisionSchema = new mongoose.Schema({
+    scheduledAt: {
+        type: Date,
+        required: true
+    },
+    completedAt: {
+        type: Date,
+        default: null
+    }
+}, { _id: false });
+
+const taskSchema = new mongoose.Schema({
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    heading: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    revisions: {
+        type: [revisionSchema],
+        required: true
+    }
+}, { timestamps: true })
+
+taskSchema.index({
+    creator:1,
+    "revisions.scheduleAt":1,
+    "revisions.completedAt":1
+})
+
+export const Task = mongoose.model("Task", taskSchema);
