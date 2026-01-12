a1bdf44415eb59471231fbba40f9ee7893ac5a9c
bhavy1204 <bhavysarwa51@gmail.com>
2026-01-12 13:36:35 +0530

    feat: add central apiClient class

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,16 +1,16 @@
-const API_BASE_URL = process.env.VITE_API_URL || "http://localhost:3000/api/v1"
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1"
 
 class ApiCLient {
     constructor() {
         this.baseURL = API_BASE_URL
     }
 
-    async request(endpoint, options) {
+    async request(endpoint, options = {}) {
         const url = `${this.baseURL}${endpoint}`
         const config = {
             ...options,
             headers: {
-                'ContentType': 'application/json',
+                'Content-Type': 'application/json',
                 ...options.headers,
             },
             credentials: "include",
@@ -30,4 +30,105 @@ class ApiCLient {
         }
 
     }
+
+
+    // AUth methods 
+
+    async register(userData) {
+        return this.request('/user/register', {
+            method: 'POST',
+            body: JSON.stringify(userData)
+        })
+    }
+
+    async login(email, password) {
+        return this.request('/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password })
+        })
+    }
+
+    async authMe() {
+        return this.request('/user/authMe', {
+            method: 'GET'
+        })
+    }
+
+    async logout() {
+        return this.request('/user/logout', {
+            method: 'POST'
+        })
+    }
+
+    async refreshToken() {
+        return this.request('/user/refresh-token', {
+            method: 'GET'
+        })
+    }
+
+    // user methods
+
+    async changePassword(oldPassword, newPassword) {
+        return this.request("/user/change-password", {
+            method: 'PATCH',
+            body: JSON.stringify({ oldPassword, newPassword })
+        })
+    }
+
+    async getUser() {
+        return this.request("/user/get-user", {
+            method: 'GET',
+        })
+    }
+
+    async deleteAccount() {
+        return this.request("/user/delete-account", {
+            method: 'DELETE'
+        })
+    }
+
+    // task methods 
+
+    async createTask(heading, link, startDate) {
+        return this.request("/task/create-task", {
+            method: 'POST',
+            body: JSON.stringify({ heading, link, startDate })
+        })
+    }
+
+    async getTodayRevision() {
+        return this.request("/task/get/today-revision", {
+            method: 'GET'
+        })
+    }
+
+    async getAllPendingRevision() {
+        return this.request("/task/get/all-pending-revision", {
+            method: 'GET'
+        })
+    }
+
+    async completeRevision(taskId) {
+        return this.request("/task/complete-revision", {
+            method: 'PATCH',
+            body: JSON.stringify({taskId})
+        })
+    }
+
+    async deleteTask(taskId) {
+        return this.request("/task/delete-task", {
+            method: 'DELETE',
+            body: JSON.stringify({taskId})
+        })
+    }
+
+    // utility
+
+    async exportToPdf(){
+        return this.request("/utility/export-to-pdf",{
+            method:'GET',
+        })
+    }
+
+
 }
